c2e9e747be20c248da2511f24d7d77f71f3cec3f
Jay <jp6631@srmist.edu.in>
2024-01-19 14:47:28 +0530

    completed ShoppingListApp

---
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.shoppinglistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.shoppinglistapp.ui.theme.ShoppingListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShoppingListAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ShoppingListApp()
+
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ShoppingListApp()
+
+    ShoppingListAppTheme {
+
+    }
+}
